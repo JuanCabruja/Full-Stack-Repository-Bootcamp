2d6c32b4889ba21e86c22dfdef9c6fc4d9d350eb
Juan MacBook <juancabrujav@gmail.com>
2021-04-06 13:58:52 +0200

    Clase JS 06/04

--- Tema_1/Ejercicio17_sesion3.psc
+++ Tema_1/Ejercicio17_sesion3.psc
@@ -1,13 +1,17 @@
 Algoritmo sin_titulo
-	definir N, contador como entero;
-	Escribir 'Introduce un número para contar hasta él.';
+	Definir N Como Entero
+	Escribir 'Inserte un número'
 	Leer N; 
 	
-	contador = 0 
-	
-	Mientras contador < N hacer 
-		contador = contador + 1
-	FinMientras
-	
-	Escribir 'Ahora contador es ', contador;
+	Si N mod 2 = 0 entonces
+		Escribir 'La ecuación es ', N,'+', N+2,'+' ,N+4,'+', N+6, '+',N+8;
+		Escribir 'El resultado es '  (N)+(N+2)+(N+4)+(N+6)+(N+8);
+		sino 
+			N = N+1 
+			Escribir 'La ecuación es ', N,'+', N+2,'+' ,N+4,'+', N+6, '+',N+8;
+			Escribir 'El resultado es '  (N)+(N+2)+(N+4)+(N+6)+(N+8)
+		
+		
+	FinSi
 FinAlgoritmo
+Leer lista_de_variables
--- Tema_5_JavaScript/Ejercicios/Ejercicio 1/js/main.js
+++ Tema_5_JavaScript/Ejercicios/Ejercicio 1/js/main.js
@@ -30,23 +30,23 @@ colorButton.addEventListener("click", function (event){
 // Apartado 3 - Con 3 URL en el HTMl cada vez que se haga click en 1 se vea.
 
 
-const divImg = document.getElementsByTagName("img")[0];
+// const divImg = document.getElementsByTagName("img")[0];
 
-const listItems = document.getElementsByTagName("li");
+// const listItems = document.getElementsByTagName("li");
 
-let changeImage = event => divImg.src = event.target.textContent;
+// let changeImage = event => divImg.src = event.target.textContent;
 
-listItems[0].addEventListener ("click", changeImage);
-listItems[1].addEventListener ("click", changeImage);
-listItems[2].addEventListener ("click", changeImage);// v2 - Autoría Juan Alberto
-const list = document.querySelector("ul");
-const image = document.querySelector("img");
-
-
-const changeImage = e => image.src = e.target.textContent;
+// listItems[0].addEventListener ("click", changeImage);
+// listItems[1].addEventListener ("click", changeImage);
+// listItems[2].addEventListener ("click", changeImage);// v2 - Autoría Juan Alberto
+// const list = document.querySelector("ul");
+// const image = document.querySelector("img");
 
 
-list.addEventListener("click", changeImage);
+// const changeImage = e => image.src = e.target.textContent;
+
+
+// list.addEventListener("click", changeImage);
 
 // v2 - Autoría Juan Alberto
 const list = document.querySelector("ul");
@@ -57,3 +57,86 @@ const changeImage = e => image.src = e.target.textContent;
 
 
 list.addEventListener("click", changeImage);
+
+// Apartado 4 -- Añadir un input de tipo texto y un botón. 
+// Al pulsar el botón debe escribirse el texto del input en un párrafo. 4.2 
+// Añadir un nuevo input pero esta vez cambiará el texto con cada pulsación de tecla del usuario.
+
+const ap4Paragraph = document.getElementById("fillableParagraph");
+const ap4Input = document.querySelector("input");
+const ap4Button = document.querySelectorAll("button")[1];
+
+ap4Button.addEventListener("click", () => {
+    ap4Paragraph.textContent = ap4Input.value;
+    ap4Input.value = "";
+})
+
+// 4.2 Añadir un nuevo input pero esta vez cambiará el texto con cada pulsación de tecla del usuario.
+
+const ap42Paragraph = document.getElementById("fillableParagraph2");
+const ap42Input = document.querySelectorAll("input")[1];
+
+ap42Input.addEventListener("input", () => {
+    ap42Paragraph.textContent = ap42Input.value;
+})
+
+// Apartado 5 -- De forma similar al anterior, pero para un textarea y validará si lo introducido es mayor de 15 caracteres
+
+
+const textarea = document.getElementById("textAreaToValidate");
+textarea.addEventListener("input", e => {
+    if (e.target.value.length < 15 ) {
+        e.target.style.backgroundColor = "red";
+    } else {
+        e.target.style.backgroundColor = "green";
+    }
+})
+
+// Apartado 6 -- Añadir un input de tipo texto con leyenda: “Escribir un número par”. 
+// Añadir un botón. Al pulsar el botón nos validará si el número es par o no. 
+// En caso negativo, cambiar los bordes del input a rojo.
+// Para revertir el estado de una propiedad, podemos utilizar el valor “revert” o dejarla vacío.
+
+// tanto el label como el Input tienen el mismo Id por tanto se consideran Hermanos y tenemos que
+// Seleccionarlo con un comando especifico. 
+
+const evenInput = document.querySelector("#evenNumberInput");
+evenInput.nextElementSibling.addEventListener("click", () => {
+    if (evenInput.value % 2 == 0) {
+        evenInput.style.border = "2px solid green"
+    } else {
+        evenInput.style.border = "2px solid red"
+    }
+})
+
+// Apartado 7 -- Partiendo de una lista ul, crear 10 li con un texto indicando el número del elemento
+// (“Elemento X”) usando un bucle for.
+
+const mainUl = document.querySelector(".mainUl");
+
+for (let i = 1; i <= 10; i++) {
+    const newLi = document.createElement("li");
+    newLi.textContent = `Elemento: ${i}`; 
+    mainUl.appendChild(newLi);
+};
+
+
+// Apartado 8 -- Crea un enlace y un botón. Si pulso el enlace se me abre la URL en la misma página. 
+// Si pulso primero el botón y luego el enlace, se me abre en una nueva pestaña.
+
+const linkArea = document.querySelector("#linkArea");
+const linkButton = document.querySelector("#linkButton")
+
+linkButton.addEventListener("click", () => {
+linkArea.toggle(target = "_blank");
+})
+
+// Apartado 9 -- Añadir un párrafo y un select con 5 opciones de colores: negro, blanco, rojo, amarillo, 
+// verde y azul. Al seleccionar un color del select, cambiar el color del párrafo.
+
+const selectChoicer = document.querySelector("#colorSenderSelect");   
+const colorReceiverParagraph = document.querySelector("#colorReceiverParagraph"); 
+
+selectChoicer.addEventListener("change", e => {
+    colorReceiverParagraph.style.color = e.target.value;
+})
